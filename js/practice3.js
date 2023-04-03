b5c9d2468bf912a79b551f85b54844410eaabd17
rhc0508 <rhc0508@naver.com>
2023-04-03 18:16:27 +0900

    fourth commit

---
+++ js/practice3.js
@@ -0,0 +1,14 @@
+const input = document.querySelector("#input");
+
+function checker() {
+  const words = document.querySelectorAll(".word");
+  if (words.length === 0) {
+    alert("Success!👏");
+    if (confirm("retry?")) {
+      window.location.reload();
+    }
+  }
+}
+
+// 여기에 코드를 작성하세요
+console.log(input.value);
